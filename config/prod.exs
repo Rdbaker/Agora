02b6aa1eb95ebe8b6b4e6288cd2e95ffc8aafdaf
Ryan Baker <ryan.da.baker@gmail.com>
2019-06-18 08:24:19 -0400

    Removes static cache manifest

--- config/prod.exs
+++ config/prod.exs
@@ -12,7 +12,6 @@ use Mix.Config
 config :agora, AgoraWeb.Endpoint,
   url: [scheme: "https", host: "agorachatapi.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
 config :logger, level: :info
